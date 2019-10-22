3993a1c393768e94de64e0af43f69edf5c57aa6e
keisuke ohashi <kog71903@gmail.com>
2019-10-22 17:03:21 +0900

    [update]react-router導入

--- app/index.js
+++ app/index.js
@@ -2,14 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Nav from './components/Nav'
+import New from './components/New'
+import Top from './components/Top'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello World!
-        <Nav />
-      </div>
+      <Router>
+        <div>
+          Hello World!
+          <Nav />
+
+          <Route exact path='/' component={Top}></Route>
+          <Route path='/new' component={New}></Route>
+        </div>
+
+      </Router>
     )
   }
 }
