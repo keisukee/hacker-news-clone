c3b7130cd8fac4ddb08ceaa4f6cfe7b4c804b682
keisuke ohashi <kog71903@gmail.com>
2019-10-23 10:34:23 +0900

    [update]Navから各ページに遷移できるように

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Nav extends React.Component {
   render() {
     return (
       <div>
-        aa
+        <ul>
+          <li>
+            <Link to='/' className='nav-link'>Top</Link>
+          </li>
+          <li>
+            <Link to='/new' className='nav-link'>New</Link>
+          </li>
+        </ul>
       </div>
     )
   }
