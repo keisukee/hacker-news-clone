424d6c4c7d6c0b17c7b9c8217a2f07932aea643a
keisuke ohashi <kog71903@gmail.com>
2019-10-23 10:34:43 +0900

    [update]ページ遷移と同時にAPIを叩きデータを表示するように

--- app/components/Article.js
+++ app/components/Article.js
@@ -5,15 +5,34 @@ export default class Article extends React.Component {
     super(props)
 
     this.state = {
-      contacts: []
+      contacts: [],
+      author: null,
+      title: null
     }
+
+  }
+  componentDidMount() {
+    fetch(`https://hacker-news.firebaseio.com/v0/item/${this.props.articleId}.json`)
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({
+          author: data.by,
+          title: data.title
+        })
+        console.log("data: ", data)
+        return data
+      })
+      .catch(console.log("error"))
   }
 
   render() {
+    console.log("props: ", this.props)
+    console.log("state: ", this.state)
     return (
       <div>
-        <h2 className="title">{this.props.title}</h2>
-        <p className="author">by {this.props.author}</p>
+        <span>{this.state.articleId}</span>
+        <h2 className="title">{this.state.title}</h2>
+        <p className="author">by {this.state.author}</p>
       </div>
     )
   }
--- app/components/New.js
+++ app/components/New.js
@@ -8,7 +8,8 @@ export default class New extends React.Component {
     super(props)
 
     this.state = {
-      ids: []
+      ids: [],
+      info: null
     }
 
     this.gatherNewsIds = this.gatherNewsIds.bind(this)
@@ -16,27 +17,39 @@ export default class New extends React.Component {
     // this.gatherNewsIds = this.gatherNewsIds.bind(this)
     // this.showNews = this.showNews.bind(this)
   }
+
+  componentDidMount() {
+    fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ ids: data })
+        console.log("aaa: ", data)
+        return data
+      })
+      .catch(console.log)
+  }
+
   gatherNewsIds() {
     fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({ ids: data })
-      console.log("aaa: ", data)
-      return data
-    })
-    .catch(console.log)
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ ids: data })
+        console.log("aaa: ", data)
+        return data
+      })
+      .catch(console.log)
 
   }
 
   getNews(id) {
     fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({ info: data })
-      console.log("data: ", data)
-      return data
-    })
-    .catch(console.log("error"))
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ info: data })
+        console.log("data: ", data)
+        return data
+      })
+      .catch(console.log("error"))
   }
 
   render() {
@@ -45,15 +58,14 @@ export default class New extends React.Component {
       <div>
         new
         <Contacts />
-        {info && <Article title={info.title} author={info.by} />}
         <button onClick={() => this.gatherNewsIds()}>gatherNewsIds</button>
         <div>
           {ids.map((id) => (
             <div key={id}>
               <span>{id}</span>
               <button onClick={() => this.getNews(id)}>show news</button>
+              <Article articleId={id} />
             </div>
-
           ))}
         </div>
       </div>
