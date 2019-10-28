78cb0e77a928c4132f83d2b0c955197f16e7994c
keisuke ohashi <kog71903@gmail.com>
2019-10-28 09:31:02 +0900

    [update]lightモードとdarkモードを切り替えできるように

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,19 +1,34 @@
 import React from 'react'
+import { ThemeConsumer } from '../contexts/theme'
 import { Link } from 'react-router-dom'
 
 export default class Nav extends React.Component {
   render() {
     return (
-      <div>
-        <ul className='nav-container'>
-          <li>
-            <Link to='/' className='nav-link'>Top</Link>
-          </li>
-          <li>
-            <Link to='/new' className='nav-link'>New</Link>
-          </li>
-        </ul>
-      </div>
+      <ThemeConsumer>
+        {({ theme, toggleTheme }) => (
+          <div>
+            <ul className='nav-container'>
+              <li>
+                <Link to='/' className='nav-link'>Top</Link>
+              </li>
+              <li>
+                <Link to='/new' className='nav-link'>New</Link>
+              </li>
+            </ul>
+            <nav>
+              <button
+                style={{fontSize: 30}}
+                onClick={toggleTheme}
+              >
+                {theme === 'light' ? '🔦' : '💡'}
+              </button>
+            </nav>
+          </div>
+
+
+        )}
+      </ThemeConsumer>
     )
   }
 }
