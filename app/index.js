a0d8935d9d94a109ca16410bdd84611852c40bb0
keisuke ohashi <kog71903@gmail.com>
2019-10-28 09:25:40 +0900

    [add]ThemeProviderを追加

--- app/index.js
+++ app/index.js
@@ -6,21 +6,38 @@ import New from './components/New'
 import Top from './components/Top'
 import Post from './components/Post'
 import User from './components/User'
+import { ThemeProvider } from './contexts/theme'
+
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
+    }
+  }
   render() {
+    console.log("theme: ", this.state.theme)
     return (
-      <Router>
-        <div>
-          <Nav />
-          <Route exact path='/' component={Top}></Route>
-          <Route path='/new' component={New}></Route>
-          <Route path='/post' component={Post}></Route>
-          <Route path='/user' component={User}></Route>
-        </div>
-      </Router>
+      <ThemeProvider value={this.state}>
+        <Router>
+          <div>
+            <Nav />
+            <Route exact path='/' component={Top}></Route>
+            <Route path='/new' component={New}></Route>
+            <Route path='/post' component={Post}></Route>
+            <Route path='/user' component={User}></Route>
+          </div>
+        </Router>
+      </ThemeProvider>
     )
   }
 }
